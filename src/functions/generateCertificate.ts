6c050b51ac558879c57da674897b169ec185236c
felpst <felipe.t.v.d@gmail.com>
2022-04-19 10:27:09 -0300

    Finally working

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -5,6 +5,7 @@ import dayjs from "dayjs";
 import { join } from "path";
 import { readFileSync } from "fs";
 import chromium from "chrome-aws-lambda";
+import { S3 } from "aws-sdk";
 
 interface ICreateCertificate {
     id: string;
@@ -32,17 +33,6 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
     const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-    document.put({
-        TableName: "users_certificate",
-        Item: {
-            id,
-            name,
-            grade,
-            created_at: new Date().getTime(),
-        },
-    })
-    .promise();
-
     const response = await document.query({
         TableName: "users_certificate",
         KeyConditionExpression: "id = :id",
@@ -50,6 +40,24 @@ export const handler: APIGatewayProxyHandler = async (event) => {
             ":id": id,
         },
     }).promise();
+
+    const userAlreadyExists = response.Items[0];
+
+    if(!userAlreadyExists) {
+
+        await document.put({
+            TableName: "users_certificate",
+            Item: {
+                id,
+                name,
+                grade,
+                created_at: new Date().getTime(),
+            },
+        })
+        .promise();
+
+    }
+
 
     const medalPath = join(process.cwd(), 'src', 'templates', 'selo.png');
     const medal = readFileSync(medalPath, 'base64');
@@ -83,9 +91,26 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
     await browser.close();
 
+    const s3 = new S3();
+
+    // await s3.createBucket({
+    //     Bucket: "ignite-course-certificate",
+    // }).promise();
+
+    await s3.putObject({
+        Bucket: "ignite-course-certificate",
+        Key: `${id}.pdf`,
+        ACL: "public-read",
+        Body: pdf,
+        ContentType: "application/pdf",
+    }).promise()
+
     return {
         statusCode: 201,
-        body: JSON.stringify(response.Items[0]),
+        body: JSON.stringify({
+            message: "Certificado criado com sucesso!",
+            url: `https://ignite-course-certificate.s3.amazonaws.com/${id}.pdf`
+        }),
     }
 
 }
